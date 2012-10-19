9adbbb3c9baa6f6af28fc50b5d7494204954a40e
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-10-19 16:59:36 +0200

    Tcp structure

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
+'use strict';
+
 var Collector = require("Netflow");
 var reloader = require('reloader');
+var ip = require('./lib/ip');
 var argv = require('optimist').argv;
+var LRU = require("lru-cache");
+
+var potentialConnections = LRU({
+    max: 500000,
+    maxAge: 1000 * 600
+});
 
 var port = argv.p || 9996;
 
-
-var tcp_flags = {};
-tcp_flags["FIN"] = 0x1 << 0;
-tcp_flags["SYN"] = 0x1 << 1;
-tcp_flags["RST"] = 0x1 << 2;
-tcp_flags["PSH"] = 0x1 << 3;
-tcp_flags["ACK"] = 0x1 << 4;
-tcp_flags["URG"] = 0x1 << 5;
-tcp_flags["ECN"] = 0x1 << 6;
-tcp_flags["CWN"] = 0x1 << 7;
-tcp_flags["NNC"] = 0x1 << 8;
 
 var app = new Collector(function (err) {
     if(err != null) {
@@ -23,38 +21,30 @@ var app = new Collector(function (err) {
 })
 .on("listening",function() { console.log("listening", port); } )
 
-.on("packet",function(packet) {
-    function flags(f) {
-        var res = {};
-        for(var k in tcp_flags) {
-            if((tcp_flags[k] & f) != 0)
-                res[k] = true;
+.on("packet",function(nflow) {
+    nflow.v5Flows.forEach(function(raw) {
+        var packet = ip.parsePacket(raw);
+//        if(packet && (packet.src == '146.48.122.92' | packet.dst == '146.48.122.92')) {
+        if(packet) {
+//            console.log("GOT PACKET FROM MYSELF", packet.isStartOfConnection, packet.flags, '0x' + packet.rawFlags.toString(16));
+
+            if(packet.isStartOfConnection) {
+                var key = packet.dstEndpoint();
+                potentialConnections.set(key, packet);
+
+//                console.log("added to cache", key);
+//                console.log("got potential start of connection. Waiting for", key);
+            } else if(packet.isAckOfConnection) {
+                var key = packet.srcEndpoint();
+                var orig = potentialConnections.get(key);
+//                console.log("checking cache", key, orig);
+                if(orig) {
+                    console.log("got connection established", orig);
+}
+            }
+        } else {
+//            console.log("unhandled ip packet", raw);
         }
-        return res;
-    }
-
-    function renderFlags(f) {
-        return Object.keys(f).join(',');
-    }
-
-    function renderIp(ips) {
-        return ips.join('.');
-    }
-
-    packet.v5Flows.forEach(function(flow) {
-        var ff = flow.tcp_flags;
-        if(ff != 0) {
-            var ffl = flags(ff);
-            if(ffl.SYN && !ffl.ACK)
-                console.log("New connection", flow.srcaddr, flow.srcport, flow.dstaddr, flow.dstport, "FLAAAGS", "0x" + ff.toString(16), renderFlags(ffl));
-
-            if(renderIp(flow.srcaddr) == '146.48.87.66' || renderIp(flow.srcaddr) == '146.48.122.92')
-                console.log("from me", renderIp(flow.srcaddr), flow.srcport, renderIp(flow.dstaddr), flow.dstport, "FLAAAGS", "0x" + ff.toString(16), renderFlags(ffl));
-
-        }
-//        else
-//            console.log("boring Packet", flow.srcaddr, flow.srcport, flow.dstaddr, flow.dstport, "FLAAAGS", "0x" + flow.tcp_flags.toString(16), flags(flow.tcp_flags));
-
     });
 });
 
