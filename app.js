8f1375e3e33a7ad41dd5debc31b054383706fb17
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-10-21 16:46:13 +0200

    Manage ephemeral ports reuse

--- app.js
+++ app.js
@@ -43,47 +43,58 @@ var app = new Collector(function (err) {
     nflow.v5Flows.forEach(function(raw) {
         var netflow = ip.parsePacket(raw, timestamp);
         if(netflow) {
+//            console.log("GOT FLOW", netflow);
+
             var unordered = netflow.unordered();
 
-            var key;
-            key = netflow.ordered() + "_flags";
-            var oldFlags = db.get(key);
-            db.set(key, (oldFlags || 0) | netflow.flags);
+            var k = unordered;
 
-            key = unordered + "_flow" ;
-            var flow;
-            if(netflow.sport > netflow.dport)
-               flow = {src: netflow.srcEndpoint(), dst: netflow.dstEndpoint()};
-            else
-               flow = {src: netflow.dstEndpoint(), dst: netflow.srcEndpoint()};
+            rclient.get("last_"+k, function(err, value) {
+                var key;
+                key = netflow.ordered() + "_flags";
+                var oldFlags = db.get(key);
 
-            db.set(key, flow);
+                if(value && (netflow.first - value) > 60000) {
+                    // REUSED EPHEMERAL PORT
+                    oldFlags = 0;
+                }
 
-            var sFlags = db.get(flow.src + "_" + flow.dst + "_flags") || 0;
-            var dFlags = db.get(flow.dst + "_" + flow.src + "_flags") || 0;
+                db.set(key, (oldFlags || 0) | netflow.flags);
 
-            var tcpFlow = new ip.TcpFlow(flow, sFlags, dFlags);
+                key = unordered + "_flow" ;
+                var flow;
+                if(netflow.sport > netflow.dport)
+                    flow = {src: netflow.srcEndpoint(), dst: netflow.dstEndpoint()};
+                else
+                    flow = {src: netflow.dstEndpoint(), dst: netflow.srcEndpoint()};
 
-            var state = tcpFlow.state();
+                db.set(key, flow);
+
+                var sFlags = db.get(flow.src + "_" + flow.dst + "_flags") || 0;
+                var dFlags = db.get(flow.dst + "_" + flow.src + "_flags") || 0;
+
+                var tcpFlow = new ip.TcpFlow(flow, sFlags, dFlags);
+
+                var state = tcpFlow.state();
 
 //            console.log("got tcp netflow " + flow.src + " -> " + flow.dst + " (0x"+sFlags.toString(16)+" 0x"+dFlags.toString(16)+") state: " + state);
 
-            var cmd = [];
-            var k = unordered;
-            cmd.push("src_"+k);
-            cmd.push(flow.src);
-            cmd.push("dst_"+k);
-            cmd.push(flow.dst);
-            cmd.push("st_"+k);
-            cmd.push(state);
-            cmd.push("last_"+k);
-            cmd.push(netflow.last);
+                var cmd = [];
+                cmd.push("src_"+k);
+                cmd.push(flow.src);
+                cmd.push("dst_"+k);
+                cmd.push(flow.dst);
+                cmd.push("st_"+k);
+                cmd.push(state);
+                cmd.push("last_"+k);
+                cmd.push(netflow.last);
 
-            rclient.mset(cmd, function() {});
+                rclient.mset(cmd, function() {});
 
-            rclient.expire("src_"+k, 60 * 10);
-            rclient.expire("dst_"+k, 60 * 10);
-            rclient.expire("st_"+k, 60 * 10);
+                rclient.expire("src_"+k, 60 * 10);
+                rclient.expire("dst_"+k, 60 * 10);
+                rclient.expire("st_"+k, 60 * 10);
+            });
         } else {
 //            console.log("unhandled ip packet", raw);
         }
