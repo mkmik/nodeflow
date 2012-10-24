46dc5bf1b073c2245704685eeb45bedbbbe992f2
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-10-24 18:19:03 +0200

    Don't try to track connections, just aggregate connectsion from an ip to a given destination service

--- sub.js
+++ sub.js
@@ -8,12 +8,6 @@ var zmq = require('zmq')
 
 sock.connect('tcp://127.0.0.1:3412');
 
-var db = LRU({
-    max: 200000,
-    maxAge: 1000 * 600
-});
-
-
 var rclient = redis.createClient();
 
 sock.on("message", function(data) {
@@ -23,59 +17,19 @@ sock.on("message", function(data) {
 
     var netflow = ip.parsePacket(raw, timestamp);
     if(netflow) {
-        //            console.log("GOT FLOW", netflow);
-        
-        var unordered = netflow.unordered();
-        
-        var k = unordered;
+        if(netflow.dport > netflow.sport) {
+            // reply
+            return;
+        } else {
+            var connectionKey = netflow.directed();
+            if(netflow.parsedFlags.SYN)
+                rclient.incr('conns_' + connectionKey);
+            rclient.incrby('packets_' + connectionKey, raw.dPkts);
+            rclient.incrby('octets_' + connectionKey, raw.dOctets);
 
-        rclient.get("last_"+k, function(err, value) {
-            var key;
-            key = netflow.ordered() + "_flags";
-            var oldFlags = db.get(key);
-
-            if(value && (netflow.first - value) > 60000) {
-                // REUSED EPHEMERAL PORT
-                oldFlags = 0;
-            }
-
-            db.set(key, (oldFlags || 0) | netflow.flags);
-
-
-            var flow;
-            if(netflow.sport > netflow.dport)
-                flow = {src: netflow.srcEndpoint(), dst: netflow.dstEndpoint()};
-            else
-                flow = {src: netflow.dstEndpoint(), dst: netflow.srcEndpoint()};
-
-            var sFlags = db.get(flow.src + "_" + flow.dst + "_flags") || 0;
-            var dFlags = db.get(flow.dst + "_" + flow.src + "_flags") || 0;
-
-            var tcpFlow = new ip.TcpFlow(flow, sFlags, dFlags);
-
-            var state = tcpFlow.state();
-
-            //            console.log("got tcp netflow " + flow.src + " -> " + flow.dst + " (0x"+sFlags.toString(16)+" 0x"+dFlags.toString(16)+") state: " + state);
-
-            var cmd = [];
-            cmd.push("src_"+k);
-            cmd.push(flow.src);
-            cmd.push("dst_"+k);
-            cmd.push(flow.dst);
-            cmd.push("st_"+k);
-            cmd.push(state);
-            cmd.push("last_"+k);
-            cmd.push(netflow.last);
-
-            rclient.mset(cmd, function() {});
-
-            rclient.expire("src_"+k, 60 * 10);
-            rclient.expire("dst_"+k, 60 * 10);
-            rclient.expire("st_"+k, 60 * 10);
-            rclient.expire("last_"+k, 60 * 10);
-        });
-    } else {
-        //            console.log("unhandled ip packet", raw);
+            rclient.expire('conns_' + connectionKey, 60 * 10);
+            rclient.expire('packets_' + connectionKey, 60 * 10);
+            rclient.expire('octets_' + connectionKey, 60 * 10);
+        }
     }
-
 });
